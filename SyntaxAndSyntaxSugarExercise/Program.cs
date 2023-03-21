86da12fcca4676d2806d9d7eacbf024eed1a7cef
Bahareh Amricachi <baharehamri@gmail.com>
2023-03-21 12:21:24 -0700

    Inferred typing

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }
         }
     }
 }
